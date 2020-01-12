c204a0d1330c1d74f7748d2298093a1186a90e5e
Vitold S <vit1251@gmail.com>
2020-01-12 20:23:32 +0300

    Fix Debian package in Circle CI service

--- package.sh
+++ package.sh
@@ -4,11 +4,13 @@ set -x
 
 # Step 1. Clean
 rm -rf ./package
+rm -rf ./dist
 
 # Step 2. Create directories
 install -m 0755 -d ./package
 install -m 0755 -d ./package/DEBIAN
 install -m 0755 -d ./package/usr/local/bin
+install -m 0755 -d ./dist
 
 # Step 3. Make Debian package description
 cp ./DEBIAN/control ./package/DEBIAN/control
@@ -18,3 +20,6 @@ cp ./golden ./package/usr/local/bin/golden
 
 # Step 5. Create Debian package
 dpkg-deb -v --build ./package golden-1.2.3.deb
+
+# Step 6. Copy Debian package in dist directory
+cp ./golden-1.2.3.deb ./dist/golden-1.2.3.deb
