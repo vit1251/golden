1a532c19adbc30566ee6a5ca174770cf5ccf3497
Vitold S <vit1251@gmail.com>
2020-04-24 04:34:01 +0300

    Update build scenaros

--- tasks.py
+++ tasks.py
@@ -14,15 +14,53 @@ def depend(c):
     c.run('go get -v -u', echo=True)
 
 @task
-def package(c, version="1.2.11"):
+def package_win(c):
+    """ Create Windows ZIP package
+    """
+    now = datetime.now()
+    stamp = now.strftime("%Y%m%d")
+    package_name = "GoldenPoint-{stamp}.zip".format(stamp=stamp)
+    #
+    platform_system_name = platform_system()
+    if platform_system_name == "Darwin":
+        pass
+    elif platform_system_name == "Windows":
+        env = {
+            "PATH": "C:\\Program Files\\7-Zip",
+        }
+        c.run('7z a -tzip {package_name} golden-windows-386.exe'.format(package_name=package_name), echo=True, env=env, warn=True)
+        c.run('7z a -tzip {package_name} golden-linux-amd64'.format(package_name=package_name), echo=True, env=env, warn=True)
+        c.run('7z a -tzip {package_name} golden-windows-amd64.exe'.format(package_name=package_name), echo=True, env=env, warn=True)
+        c.run('7z a -tzip {package_name} README.md'.format(package_name=package_name), echo=True, env=env, warn=True)
+        c.run('7z a -tzip {package_name} LICENSE'.format(package_name=package_name), echo=True, env=env, warn=True)
+        c.run('7z a -tzip {package_name} static'.format(package_name=package_name), echo=True, env=env, warn=True)
+    elif platform_system_name == "Linux":
+        c.run('zip {package_name} golden-windows-386.exe'.format(package_name=package_name), echo=True, warn=True)
+        c.run('zip {package_name} golden-linux-amd64'.format(package_name=package_name), echo=True, warn=True)
+        c.run('zip {package_name} golden-windows-amd64.exe'.format(package_name=package_name), echo=True, warn=True)
+        c.run('zip {package_name} README.md'.format(package_name=package_name), echo=True, warn=True)
+        c.run('zip {package_name} LICENSE'.format(package_name=package_name), echo=True, warn=True)
+        c.run('zip -r {package_name} static'.format(package_name=package_name), echo=True, warn=True)
+    else:
+        raise RuntimeError('Unknown system {platform_system_name}'.format(platform_system_name=platform_system_name))
+
+
+@task
+def package_linux(c, version="1.2.11-1"):
+    """ Create Debian DEB package
+    """
     c.run('install -m 0755 -d ./package')
     c.run('install -m 0755 -d ./package/DEBIAN')
     c.run('install -m 0755 -d ./package/usr/local/bin')
     c.run('install -m 0755 -d ./dist')
     c.run('cp ./DEBIAN/control ./package/DEBIAN/control')
-    c.run('cp ./golden ./package/usr/local/bin/golden')
-    c.run('dpkg-deb -v --build ./package golden-{version}.deb'.format(version=version))
-    c.run('cp ./golden-{version}.deb ./dist/golden-{version}.deb'.format(version=version))
+    c.run('cp ./golden-linux-amd64 ./package/usr/local/bin/golden')
+    c.run('dpkg-deb -v --build ./package golden_{version}_amd64.deb'.format(version=version))
+
+@task
+def package(c):
+    package_win(c)
+    package_linux(c)
 
 @task
 def check(c):
@@ -44,6 +82,17 @@ def build_w32(c):
             "CGO_ENABLED": "1",
         }
         c.run('go build -o golden-windows-386.exe .', echo=True, env=env)
+    elif platform_system_name == "Linux":
+        env = {
+            "GOOS": "windows",
+            "GOARCH": "386",
+            "CGO_ENABLED": "1",
+            "CC": "i686-w64-mingw32-gcc",
+            "CXX": "i686-w64-mingw32-g++",
+        }
+        c.run('go build -o golden-windows-386.exe .', echo=True, env=env)
+    else:
+        raise RuntimeError('Unknown system {platform_system_name}'.format(platform_system_name=platform_system_name))
 
 @task
 def build_w64(c):
@@ -97,36 +146,6 @@ def build(c):
     build_linux(c)
     build_darwin(c)
 
-@task
-def package(c):
-    """ Create 
-    """
-    now = datetime.now()
-    stamp = now.strftime("%Y%m%d")
-    package_name = "GoldenPoint-{stamp}.zip".format(stamp=stamp)
-    #
-    platform_system_name = platform_system()
-    if platform_system_name == "Darwin":
-        pass
-    elif platform_system_name == "Windows":
-        env = {
-            "PATH": "C:\\Program Files\\7-Zip",
-        }
-        c.run('7z a -tzip {package_name} golden-windows-386.exe'.format(package_name=package_name), echo=True, env=env, warn=True)
-        c.run('7z a -tzip {package_name} golden-linux-amd64'.format(package_name=package_name), echo=True, env=env, warn=True)
-        c.run('7z a -tzip {package_name} golden-windows-amd64.exe'.format(package_name=package_name), echo=True, env=env, warn=True)
-        c.run('7z a -tzip {package_name} README.md'.format(package_name=package_name), echo=True, env=env, warn=True)
-        c.run('7z a -tzip {package_name} LICENSE'.format(package_name=package_name), echo=True, env=env, warn=True)
-        c.run('7z a -tzip {package_name} static'.format(package_name=package_name), echo=True, env=env, warn=True)
-    elif platform_system_name == "Linux":
-        c.run('zip {package_name} golden-windows-386.exe'.format(package_name=package_name), echo=True, warn=True)
-        c.run('zip {package_name} golden-linux-amd64'.format(package_name=package_name), echo=True, warn=True)
-        c.run('zip {package_name} golden-windows-amd64.exe'.format(package_name=package_name), echo=True, warn=True)
-        c.run('zip {package_name} README.md'.format(package_name=package_name), echo=True, warn=True)
-        c.run('zip {package_name} LICENSE'.format(package_name=package_name), echo=True, warn=True)
-        c.run('zip -r {package_name} static'.format(package_name=package_name), echo=True, warn=True)
-    else:
-        raise RuntimeError('Unknown system {platform_system_name}'.format(platform_system_name=platform_system_name))
 
 @task
 def debug(c):
