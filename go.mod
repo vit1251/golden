6e3e62244f99ac7f7fc39f8ca94b4b62d24e25b0
Vitold S <vit1251@gmail.com>
2020-10-20 04:52:48 +0300

    Time zone parser crashes on empty kludges #41

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.12
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/faiface/beep v1.0.2
-	github.com/gliderlabs/ssh v0.3.0
+	github.com/gliderlabs/ssh v0.3.1
 	github.com/gorilla/mux v1.8.0
 	github.com/hajimehoshi/go-mp3 v0.3.1 // indirect
 	github.com/hajimehoshi/oto v0.6.5 // indirect
@@ -15,9 +15,9 @@ require (
 	github.com/xeonx/timeago v1.0.0-rc4
 	go.uber.org/dig v1.10.0
 	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
-	golang.org/x/exp v0.0.0-20200924195034-c827fd4f18b9 // indirect
+	golang.org/x/exp v0.0.0-20201008143054-e3b2a7f2fdc7 // indirect
 	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
 	golang.org/x/mobile v0.0.0-20200801112145-973feb4309de // indirect
-	golang.org/x/sys v0.0.0-20201005172224-997123666555 // indirect
+	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
 	golang.org/x/text v0.3.3
 )
--- pkg/fidotime/TimeZoneParser_test.go
+++ pkg/fidotime/TimeZoneParser_test.go
@@ -5,16 +5,47 @@ import (
 	"testing"
 )
 
-// Thanks:
-// - Nil Alexandrov for message 5e4f062a for research from 21 Feb 20  05:18:17 in 0700 zone
-
-func TestTimeZoneParser(t *testing.T) {
-
+func TestTimeZoneParser0(t *testing.T) {
 	zp := NewTimeZoneParser()
-	zone, err1 := zp.Parse("0700")
+	zone, err1 := zp.Parse("0300")
+	log.Printf("zone = %q", zone)
 	if err1 != nil {
 		t.Errorf("Wrong timze zone: err1 = %+v", err1)
 	}
-	log.Printf("zone = %+v", zone)
+}
 
+func TestTimeZoneParser1(t *testing.T) {
+	zp := NewTimeZoneParser()
+	zone, err1 := zp.Parse("-0700")
+	log.Printf("zone = %q", zone)
+	if err1 != nil {
+		t.Errorf("Wrong timze zone: err1 = %+v", err1)
+	}
+}
+
+func TestTimeZoneParser2(t *testing.T) {
+	zp := NewTimeZoneParser()
+	zone, err1 := zp.Parse("-0700 ")
+	log.Printf("zone = %q", zone)
+	if err1 == nil {
+		t.Errorf("Wrong timze zone: err1 = %+v", err1)
+	}
+}
+
+func TestTimeZoneParser3(t *testing.T) {
+    zp := NewTimeZoneParser()
+	zone, err1 := zp.Parse(" ")
+	log.Printf("zone = %q", zone)
+	if err1 == nil {
+		t.Errorf("Wrong timze zone: err1 = %+v", err1)
+	}
+}
+
+func TestTimeZoneParser4(t *testing.T) {
+	zp := NewTimeZoneParser()
+	zone, err1 := zp.Parse("MSK")
+	log.Printf("zone = %q", zone)
+	if err1 == nil {
+		t.Errorf("Wrong timze zone: err1 = %+v", err1)
+	}
 }
