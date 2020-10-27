af6a324cc00b15d8d58b634b8ef70bc8b1bf7011
Vitold S <vit1251@gmail.com>
2020-10-27 15:04:02 +0300

    Remove default value in initial setup

--- pkg/installer/Migration_20201023000102.go
+++ pkg/installer/Migration_20201023000102.go
@@ -2,14 +2,13 @@ package installer
 
 import (
 	"database/sql"
-	"path"
 )
 
 func migration_000102_Up(conn *sql.DB) error {
 
-	query1 := "INSERT INTO `settings` (`section`,`name`,`value`) VALUES (?,?,?)"
+	query1 := "INSERT INTO `settings` (`section`,`name`,`value`) VALUES (?, ?, ?)"
 
-	if _, err := conn.Exec(query1, "main", "RealName", "Alice Cooper"); err != nil {
+	if _, err := conn.Exec(query1, "main", "RealName", "John Smith"); err != nil {
 	}
 
 	if _, err := conn.Exec(query1, "main", "Country", "Russia"); err != nil {
@@ -24,37 +23,31 @@ func migration_000102_Up(conn *sql.DB) error {
 	if _, err := conn.Exec(query1, "main", "TearLine", "Golden/{GOLDEN_PLATFORM}-{GOLDEN_ARCH} {GOLDEN_VERSION} {GOLDEN_RELEASE_DATE} ({GOLDEN_RELEASE_HASH})"); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "Address", "2:5030/1592.15"); err != nil {
+	if _, err := conn.Exec(query1, "main", "Address", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "Link", "2:5030/1592.0"); err != nil {
+	if _, err := conn.Exec(query1, "main", "Link", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "NetAddr", "f24.n5023.z2.binkp.net:24554"); err != nil {
+	if _, err := conn.Exec(query1, "main", "NetAddr", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "Inbound", path.Join(".", "Inbound")); err != nil {
+	if _, err := conn.Exec(query1, "main", "Inbound", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "Outbound", path.Join(".", "Outbound")); err != nil {
+	if _, err := conn.Exec(query1, "main", "Outbound", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "TempInbound", path.Join(".", "TempInbound")); err != nil {
+	if _, err := conn.Exec(query1, "main", "TempInbound", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "TempOutbound", path.Join(".", "TempOutbound")); err != nil {
+	if _, err := conn.Exec(query1, "main", "TempOutbound", ""); err != nil {
 	}
 
-	if _, err := conn.Exec(query1, "main", "FileBox", path.Join(".", "Files")); err != nil {
+	if _, err := conn.Exec(query1, "main", "FileBox", ""); err != nil {
 	}
 
 	if _, err := conn.Exec(query1, "main", "Password", ""); err != nil {
-	}
-
-	if _, err := conn.Exec(query1, "main", "Nodelist", path.Join(".", "Nodelist")); err != nil {
-	}
-
-	if _, err := conn.Exec(query1, "main", "Pointlist", path.Join(".", "Pointlist")); err != nil {
 	}
 
 	return nil
