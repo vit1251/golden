2347c2b86047461f5516f28ecd3c29e1dbe42293
Vitold S <vit1251@gmail.com>
2020-11-05 03:46:13 +0300

    File input in TIC area

--- pkg/site/widgets/FormWidget.go
+++ pkg/site/widgets/FormWidget.go
@@ -29,11 +29,12 @@ func (self *FormWidget) SetWidget(w IWidget) *FormWidget {
 
 func NewFormWidget() *FormWidget {
 	fw:= new(FormWidget)
+	fw.enctype = "application/x-www-form-urlencoded"
 	return fw
 }
 
 func (self *FormWidget) Render(w http.ResponseWriter) error {
-	fmt.Fprintf(w, "<form action=\"%s\" method=\"%s\">\n", self.Action, self.Method)
+	fmt.Fprintf(w, "<form action=\"%s\" method=\"%s\" enctype\"%s\">\n", self.Action, self.Method, self.enctype)
 	if self.Widget != nil {
 		self.Widget.Render(w)
 	}
