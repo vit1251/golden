e8b568be3344f948a770ece9c07eb8fbbe63ff6a
Vitold S <vit1251@gmail.com>
2022-03-18 05:15:24 +0300

    - Message preview HTML escape issue (#135)

--- pkg/msg/MessageReader.go
+++ pkg/msg/MessageReader.go
@@ -1,14 +1,30 @@
 package msg
 
 import (
+	"fmt"
+	"html"
 	"html/template"
 	"log"
 	"strings"
 )
 
+type MessageDocumentElement interface {
+}
+
+type MessageDocumentQuote struct {
+	MessageDocumentElement
+	authorName string
+	quoteDeep  int
+	origText   string
+}
+
+type MessageDocumentText struct {
+	MessageDocumentElement
+	origText string
+}
+
 type MessageDocument struct {
-	html string
-	raw  string
+	items []MessageDocumentElement
 }
 
 type MessageTextProcessor struct {
@@ -27,26 +43,27 @@ func (self *MessageTextProcessor) ParseQuoteLine(quoteLine string) (author strin
 
 }
 
-func (self *MessageTextProcessor) processHtmlLine(oneLine string) string {
+func (self *MessageTextProcessor) processHtmlLine(oneLine string) MessageDocumentElement {
 
 	log.Printf("oneLine = %s", oneLine)
 
 	mlp := NewMessageLineParser()
 	ml := mlp.Parse(oneLine)
 
-	var newLine string
-
 	if ml.QuoteLevel == 0 {
-		newLine = ml.PureLine
-	} else {
-		if ml.QuoteLevel%2 == 0 {
-			newLine = "<span style='color: red'>" + ml.QuoteStart + ml.QuoteAuthor + ml.QuoteMarkers + ml.QuoteLine + "</span>"
-		} else {
-			newLine = "<span style='color: green'>" + ml.QuoteStart + ml.QuoteAuthor + ml.QuoteMarkers + ml.QuoteLine + "</span>"
+		result := MessageDocumentText{
+			origText: ml.PureLine,
 		}
+		return result
+	} else {
+		result := MessageDocumentQuote{
+			authorName: ml.QuoteAuthor,
+			quoteDeep:  ml.QuoteLevel,
+			origText:   ml.QuoteLine,
+		}
+		return result
 	}
 
-	return newLine
 }
 
 func (self *MessageTextProcessor) Prepare(msg string) (*MessageDocument, error) {
@@ -60,19 +77,79 @@ func (self *MessageTextProcessor) Prepare(msg string) (*MessageDocument, error) 
 	/* Process */
 	rows := strings.Split(msg, "\r")
 	for _, oneLine := range rows {
-		var newHtmlLine string = self.processHtmlLine(oneLine)
-		var newLine = oneLine
-		doc.html += newHtmlLine + "<br>"
-		doc.raw += newLine + "\r"
+		newElement := self.processHtmlLine(oneLine)
+		doc.Add(newElement)
 	}
 
 	return doc, nil
 }
 
 func (self *MessageDocument) HTML() template.HTML {
-	return template.HTML(self.html)
+	r := NewMessageDocumentRenderer()
+	return r.RenderAsHTML(self)
 }
 
 func (self *MessageDocument) Content() string {
-	return self.raw
+	r := NewMessageDocumentRenderer()
+	return r.RenderAsText(self)
+}
+
+func (self *MessageDocument) Add(element MessageDocumentElement) {
+	self.items = append(self.items, element)
+}
+
+type MessageDocumentRenderer struct {
+}
+
+func NewMessageDocumentRenderer() *MessageDocumentRenderer {
+	return new(MessageDocumentRenderer)
+}
+
+func (self *MessageDocumentRenderer) RenderAsText(doc *MessageDocument) string {
+
+	var items []string
+	for _, item := range doc.items {
+		switch v := item.(type) {
+		case MessageDocumentText:
+			newLine := fmt.Sprintf("%s", v.origText)
+			items = append(items, newLine)
+		case MessageDocumentQuote:
+			var padding string
+			for i := 0; i < v.quoteDeep; i++ {
+				padding = padding + ">"
+			}
+			newLine := fmt.Sprintf(" %s%s%s", v.authorName, padding, v.origText)
+			items = append(items, newLine)
+		}
+	}
+
+	return strings.Join(items, "\r")
+}
+
+func (self *MessageDocumentRenderer) RenderAsHTML(doc *MessageDocument) template.HTML {
+
+	var items []string
+	for _, item := range doc.items {
+		switch v := item.(type) {
+		case MessageDocumentText:
+			newText := html.EscapeString(v.origText)
+			newLine := fmt.Sprintf("%s", newText)
+			items = append(items, newLine)
+		case MessageDocumentQuote:
+			var padding string
+			for i := 0; i < v.quoteDeep; i++ {
+				padding = padding + ">"
+			}
+			var newLine string
+			newText := html.EscapeString(v.origText)
+			if v.quoteDeep%2 == 0 {
+				newLine = "<span style='color: red'>" + v.authorName + padding + newText + "</span>"
+			} else {
+				newLine = "<span style='color: green'>" + v.authorName + padding + newText + "</span>"
+			}
+			items = append(items, newLine)
+		}
+	}
+	var out string = strings.Join(items, "<br>")
+	return template.HTML(out)
 }
