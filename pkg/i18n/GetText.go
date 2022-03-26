5d58a12199dd90a51380c1fd23ccc887c6832392
Rudi Timmermans <101011119+ruditimmermans@users.noreply.github.com>
2022-03-26 22:01:00 +0100

    Update GetText.go

--- pkg/i18n/GetText.go
+++ pkg/i18n/GetText.go
@@ -167,7 +167,7 @@ func parseLang(lang string) Lang {
 
 func GetDefaultLanguage() string {
 
-	var result string = "en-US", "nl-BE"
+	var result string = "nl-BE"
 
 	// LANG=ru_RU.UTF-8
 	if lang, exists := os.LookupEnv("LANG"); exists {
