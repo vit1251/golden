7dcc308af6c7393f94ad8c2986a6f82052b95c55
Rudi Timmermans <101011119+ruditimmermans@users.noreply.github.com>
2022-03-31 04:37:22 +0200

    Use the correct Dutch words into translation

--- pkg/i18n/GetText.go
+++ pkg/i18n/GetText.go
@@ -106,13 +106,13 @@ func init() {
 		englishTranslation := mainTranslation.GetLangTranslation("nl-BE")
 		/* NetmailIndexAction */
 		netmailIndexActionTranslation := englishTranslation.GetActionTranslation("NetmailIndexAction")
-		netmailIndexActionTranslation.SetTranslation("action-button-create", "Maak een nieuw bericht")
+		netmailIndexActionTranslation.SetTranslation("action-button-create", "Nieuw bericht")
 		/* EchoAreaIndexAction */
 		echoAreaIndexActionTranslation := englishTranslation.GetActionTranslation("EchoAreaIndexAction")
-		echoAreaIndexActionTranslation.SetTranslation("action-button-create", "Maak een nieuw bericht")
+		echoAreaIndexActionTranslation.SetTranslation("action-button-create", "Maak een nieuw gebied")
 		/* EchoMsgIndexAction */
 		echoMsgIndexActionTranslation := englishTranslation.GetActionTranslation("EchoMsgIndexAction")
-		echoMsgIndexActionTranslation.SetTranslation("action-compose-button", "Maak een nieuw bericht")
+		echoMsgIndexActionTranslation.SetTranslation("action-compose-button", "Nieuw bericht")
 		echoMsgIndexActionTranslation.SetTranslation("action-tree-button", "Boomstructuur")
 		echoMsgIndexActionTranslation.SetTranslation("action-mark-as-read-button", "Markeer als gelezen")
 		echoMsgIndexActionTranslation.SetTranslation("action-settings-button", "Instellingen")
