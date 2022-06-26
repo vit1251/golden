d1ac739cadce7a8d673fb6acea0ea8603f5c17ec
Rudi Timmermans <101011119+ruditimmermans@users.noreply.github.com>
2022-06-26 12:04:06 +0200

    Translate missing strings to Dutch

--- pkg/i18n/Dutch.go
+++ pkg/i18n/Dutch.go
@@ -4,7 +4,19 @@ func init() {
 
 	mainTranslation := GetMainTranslation()
 
-	englishTranslation := mainTranslation.GetLangTranslation("nl-BE")
+	dutchTranslation := mainTranslation.GetLangTranslation("nl-BE")
+
+	/* Main menu */
+	actionTranslation := englishTranslation.GetActionTranslation("Actie")
+	actionTranslation.SetTranslation("menu-home", "Thuis")
+	actionTranslation.SetTranslation("menu-netmail", "Netmail")
+	actionTranslation.SetTranslation("menu-echomail", "Echomail")
+	actionTranslation.SetTranslation("menu-files", "Bestanden")
+	actionTranslation.SetTranslation("menu-service", "Diensten")
+	actionTranslation.SetTranslation("menu-people", "Adresboek)
+	actionTranslation.SetTranslation("menu-draft", "Voorlopige versie")
+	actionTranslation.SetTranslation("menu-setup", "Instellingen")
+
 	/* NetmailIndexAction */
 	netmailIndexActionTranslation := englishTranslation.GetActionTranslation("NetmailIndexAction")
 	netmailIndexActionTranslation.SetTranslation("action-button-create", "Nieuw bericht")
@@ -18,4 +30,10 @@ func init() {
 	echoMsgIndexActionTranslation.SetTranslation("action-mark-as-read-button", "Markeer als gelezen")
 	echoMsgIndexActionTranslation.SetTranslation("action-settings-button", "Instellingen")
 
+	/* FileEchoIndexAction */
+	fileEchoIndexActionTranslation := englishTranslation.GetActionTranslation("FileEchoIndexAction")
+	fileEchoIndexActionTranslation.SetTranslation("action-button-create", "Creëren")
+
 }
+
+
